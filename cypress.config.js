f0e0e02e005e814cd44cc2c55b7ae7287a321e96
OlayideAde <laide.ademeso@gmail.com>
2023-11-08 19:07:34 +0100

    Add base url

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
     },
-    baseUrl: "https://",
+    baseUrl: "https://gift-cards.phorest.com/salons/demous#",
     specPattern: ['cypress/e2e/tests/*.js'],
     reporter: "cypress-mochawesome-reporter",
     reporterOptions: {
